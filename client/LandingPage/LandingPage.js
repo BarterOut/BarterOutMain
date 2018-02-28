afee55e8ab611c87ce62b9ce90976516a2432f02
LudwigNova <lnova@u.rochester.edu>
2018-02-28 18:35:12 -0500

    About 65-70% of the way done

--- client/LandingPage/LandingPage.js
+++ client/LandingPage/LandingPage.js
@@ -5,6 +5,11 @@ import {
 import { connect } from 'react-redux'
 
 import styles from './landingpage.css';
+import regularFont from './sylesheetOrkneyRegular.css';
+import lightFont from './sylesheetOrkneyLight.css';
+import mediumFont from './sylesheetOrkneyMedium.css';
+import boldfont from './sylesheetOrkneyBold.css';
+import animations from './animate.css';
 
 const LandingPage = () => {
     return (
@@ -13,101 +18,154 @@ const LandingPage = () => {
                 <nav className="headerBar">
                     BarterOut
                 </nav>
-                <div className="mainText">
+                <div className="mainText animated fadeIn" >
                     <h1>EVERY BOOK
+                        <br/>
                         YOU WILL EVER NEED,
+                        <br/>
                         HASSLE-FREE</h1>
-                    <button className="firstButton">JOIN NOW FREE</button>
+                    <button className="barterOutButton">JOIN NOW FREE</button>
                 </div>
                 <div className="madeSimple">
-                    <div className="madeSimpleText">
-                        BUYING AND SELLING MADE SIMPLE
-                        <p>
-                            At BarterOut, we know textbooks can get very expensive and finding second-hand ones can be a hassle.
-                            That's why we make it easier for you to get your books and get to class, all with just a couple
-                            clicks on our platform. Enter some simple information and let our automated matching algorithm do
-                            the magic!
-                        </p>
-                        <br/>
-                        Buy and sell with us in minutes!
+                    <div class="animated fadeIn">
+                        <h2>BUYING AND SELLING MADE SIMPLE</h2>
+                        <div className="madeSimpleText animated fadeInUp">
+                            <p>
+                                At BarterOut, we know textbooks can get very expensive and finding second-hand ones can be a hassle.
+                                That's why we make it easier for you to get your books and get to class, all with just a couple
+                                clicks on our platform. Enter some simple information and let our automated matching algorithm do
+                                the magic!
+                            </p>
+                            <br/>
+                            Buy and sell with us in minutes!
+                        </div>
                     </div>
                 </div>
                 <div className="howItWorks">
-                    <div className="howItWorksText">
-                        HOW IT WORKS
-                        <br/>
-                        STEP 1
-                        <p>
-                        Tell us a little about you. Where do you go to school, what classes are you taking and what classes
-                        you plan to take next semester.
-                        </p>
+                    <div className="howItWorksText animated fadeInLeft">
+                        <h2 class="animated fadeInLeft">HOW IT WORKS</h2>
+                        <div className="howItWorksBodyText">
+                            <br/>
+                            <div className="steps">
+                                STEP 1
+                            </div>
+                            <p>
+                                Tell us a little about you. Where do you go to school? What classes are you taking and what classes
+                                do you want to take next semester?
+                            </p>
 
-                        <br/>
-                        STEP 2
-                        <p>
-                        We tell you which books you need for that courses and you tell us which of those you already have
-                        and which you want to buy. The books you have from last semester? Let us know if you are willing to
-                        sell them.
-                        </p>
+                            <br/>
+                            <div className="steps">
+                                STEP 2
+                            </div>
+                            <p>
+                                We tell you which books you need for your courses, and you tell us which ones you already have
+                                and which ones you want to buy. Those books you still have from last semester? Let us know
+                                if you want to to sell them.
+                            </p>
 
-                        <br/>
-                        STEP 3
-                        <p>
-                        We match buyers and sellers at the best price and delver the textbooks for you. All the payments
-                        are made securely online.
-                        </p>
+                            <br/>
+                            <div className="steps">
+                                STEP 3
+                            </div>
+                            <p>
+                                We match buyers and sellers at the best price handle the textbook delivery for you. All the payments
+                                are made securely online.
+                            </p>
+                        </div>
                     </div>
                 </div>
                 <div className="whatWereAbout">
-                    <div className="whatWereAboutText">
-                        WHAT WE'RE ABOUT
+                    <div>
+                        <h2 class="animated fadeIn" id="whatWereAboutHeader">WHAT WE'RE ABOUT</h2>
                         <br/>
-                        SMART BOOK-SHOPPING FOR SMART PEOPLE
-                        <p>
-                        Leave it to our smart search system to meet your needs and do the work for you. Why? So you can have
-                        more time to spend living that beautiful college life!
-                        </p>
+                        <div className="blurbBlock  animated fadeInLeft">
+                            <div className="smartPeopleBlock">
+                                <h2 className=" animated fadeInLeft">SMART SHOPPING FOR SMART PEOPLE</h2>
+                                <div className="whatWereAboutText">
+                                    <p>
+                                        Leave it to our smart search system to meet your needs and do the work for you. Why? So you can have
+                                        more time to spend living that beautiful college life!
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="slideInPhotoRight">
+                            </div>
+                        </div>
 
                         <br/>
-                        WHILE HELPING OTHERS
-                        <p>
-                        Every book sold or bought on BarterOut turns into an opportunity for an underprivileged student. We
-                        give back 30% of our profits as free credit on the platform for financially disadvantaged students so
-                        they can get textbooks for FREE.
-                        </p>
+                        <div className="blurbBlock  animated fadeInRight">
+                            <div className="helpingOthersBlock">
+                                <h2 className=" animated fadeInRight">WHILE HELPING OTHERS</h2>
+                                <div className="whatWereAboutText">
+                                    <p>
+                                        Every book sold or bought on BarterOut turns into an opportunity for an underprivileged student. We
+                                        give back 30% of our profits as free credit on the platform for financially disadvantaged students so
+                                        they can get textbooks for FREE.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="slideInPhotoLeft">
+                            </div>
+                        </div>
 
                         <br/>
-                        EASY, FAST & CHEAP
-                        <p>
-                        Because we care about you, we have built an easy-to-use system capable of providing you with the
-                        textbooks you need as fast as possible. In order to make this dream a reality we take a small share
-                        of every transaction to keep our servers running.
-                        </p>
+                        <div className="blurbBlock animated fadeInLeft">
+                            <div className="fastCheapEasyBlock">
+                                <h2 className=" animated fadeInLeft">EASY, FAST & CHEAP</h2>
+                                <div className="whatWereAboutText">
+                                    <p>
+                                        Because we care about you, we have built an easy-to-use system capable of providing you with the
+                                        textbooks you need as fast as possible. In order to make this dream a reality we take a small share
+                                        of every transaction to keep our servers running.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="slideInPhotoRight">
+                            </div>
+                        </div>
+                    </div>
+                    <div className="blurbBlock">
+                        <button className="barterOutButton animated fadeIn">JOIN TODAY</button>
                     </div>
                 </div>
                 <div className="teamPage">
-                    <div className="teamPageText">
-                        MEET OUR TEAM
-                        <br/>
-                        A company created by students with the help of other students, all to benefit... well... students
-                        <br/>
-                        Interested in working with us? Apply now!
+                    <div class="animated fadeIn">
+                        <h2>MEET OUR TEAM</h2>
+                        <div className="teamPageText">
+                            <div class="animated fadeInUp">
+                                A company created by students with the help of other students, all to benefit... well... students
+                                <br/>
+                                Interested in working with us? Apply now!
+                            </div>
+                        </div>
 
                         <br/>
-                        AND OUR ADVISERS
-                        <br/>
-                        Because any team needs some guidance from time to time
-                        <br/>
-                        Interested in helping us grow? Apply now!
+                        <h2>AND OUR ADVISERS</h2>
+                        <div className="teamPageText">
+                            <div class="animated fadeInUp">
+                                Because any team needs some guidance from time to time
+                                <br/>
+                                Interested in helping us grow? Apply now!
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="registrationPage">
-                    <div className="registrationPageText">
-                        THE WAIT IS OVER, JOIN NOW
-                        <p>
-                            Our algorithm is currently under construction and should be live in late May 2018. Take one
-                            task out of you to-do list and create a pre-account for FREE today!
-                        </p>
+                    <div className="registrationPageOverlay">
+                        <div className="registrationPageText animated fadeInLeft">
+                            <h2>THE WAIT IS OVER, JOIN NOW</h2>
+                            <div className="registrationPageBodyText">
+                                <p>
+                                    Our algorithm is currently under construction and should be live in late May 2018. Take one
+                                    task out of you to-do list and create a pre-account for FREE today!
+                                </p>
+                            </div>
+                        </div>
+                        <div className="registrationForm animated fadeIn">
+                            Form For filling out
+                            <button className="barterOutButton animated fadeIn" id="registerButton">Pre-Register</button>
+                        </div>
                     </div>
                 </div>
                 <div className="landingPageBottom">
