03a97cb7639844df4f7861c261cd6fc1985203a5
LudwigNova <lnova@u.rochester.edu>
2018-03-07 11:36:49 -0500

    another port change

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 const config = {
   mongoURL: process.env.MONGO_URL || 'mongodb://barterout:barterout@ds241658.mlab.com:41658/test_barterout',
-  port: process.env.PORT || 8000,
+  port: process.env.PORT || 8080,
 };
 
 export default config;
