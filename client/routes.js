4eb4fb055c7325d0a726e313d012d3b3d46fb2b1
LudwigNova <lnova@u.rochester.edu>
2018-03-12 05:35:09 -0400

    Fixed route links

--- client/routes.js
+++ client/routes.js
@@ -7,10 +7,10 @@ import {
 import Welcome from './Welcome/Welcome';
 import Mern from './Mern/Mern';
 import LandingPage from './LandingPage/LandingPage';
-import TermsOfService from './TermsOfService/TermsOfService';
-import PrivacyPolicy from './PrivacyPolicy/PrivacyPolicy';
-import Contact from './Contact/Contact'
-import Careers from './Careers/Careers';
+import TermsOfService from './TermsOfService/termsOfService';
+import PrivacyPolicy from './PrivacyPolicy/privacyPolicy';
+import Contact from './Contact/contact'
+import Careers from './Careers/careers';
 
 import store from './store'
 
