a3c2ab66a6bcfcdf54e2fa85a08c31592cd48022
DMH <dmunoz2@u.rochester.edu>
2018-06-16 19:08:52 -0400

    Email verification, needs testing but it seems to be "done"

--- server/routes/user.js
+++ server/routes/user.js
@@ -6,6 +6,101 @@ const router = express.Router();
 
 const nodemailer = require('nodemailer');
 
+// Start of email verification changes
+
+const bcrypt = require('bcryptjs');
+
+
+import mongoose from 'mongoose';
+
+const nev = require('email-verification')(mongoose);
+// Makes the email configuration settings
+nev.configure({
+  verificationURL: 'https://www.barterout.com/api/auth/email-verification/${URL}',
+  persistentUserModel: User,
+  tempUserCollection: 'barterOut_tempusers',
+
+  transportOptions: {
+    host: 'smtp.gmail.com',
+    auth: {
+      type: 'OAuth2',
+      clientId: '878736426892-d0vbth6ho78opo916rr1bimlmuufq25e.apps.googleusercontent.com',
+      clientSecret: '5OTf_iLhmt0tjJCKIdnuC5XM',
+    },
+  },
+  verifyMailOptions: {
+    from: '"Barter Out" <office@barterout.com',
+    subject: 'Please confirm account',
+    html: 'Click the following link to confirm your account:</p><p>${URL}</p>',
+    text: 'Please confirm your account by clicking the following link: ${URL}',
+    auth: {
+      user: 'office@barterout.com',
+      refreshToken: '1/9XdHU4k2vwYioRyAP8kaGYfZXKfp_JxqUwUMYVJWlZs',
+      accessToken: 'ya29.GluwBeUQiUspdFo1yPRfzFMWADsKsyQhB-jgX3ivPBi5zcIldvyPYZtRME6xqZf7UNzkXzZLu1fh0NpeO11h6mwS2qdsL_JREzpKw_3ebOWLNgxTyFg5NmSdStnR',
+    },
+  },
+  hashingFunction: myHasher, // This might break the log in for the new users as it might be hashing the hash.
+  emailFieldName: 'emailAddress',
+  passwordFieldName: 'password',
+
+}, function(error, options){
+});
+// Creating the temp user
+nev.generateTempUserModel(User);
+
+// Hashing function
+// async version of hashing function
+var myHasher = function(password, tempUserData, insertTempUser, callback) {
+  var hash = bcrypt.hashSync(password, 10, null);
+  return insertTempUser(hash, tempUserData, callback);
+};
+
+router.get('/email-verification/:URL', (req, res) => {
+
+  var url = req.params.URL;
+  console.log(url);//gonna haave to do some parsing here, or maybe not
+  nev.confirmTempUser(url, function(err, user) {
+    if (err)
+    // handle error...
+      console.log(err)
+    // user was found!
+      if (user) {
+        // optional
+        nev.sendConfirmationEmail(user['emailAddress'], function(error, info) {
+          res.redirect('/api/auth/login');
+          // redirect to their profile...
+        });
+      }
+      // user's data probably expired...
+      else{
+        console.log('user data probably expired, send some sort of msg');
+        res.redirect('api/auth/signup');
+      }
+      // redirect to sign-up
+        });
+
+});
+
+router.post('/resendEmailVerification', ( req, res ) => {
+  var email = req.emailAddress;
+  nev.resendVerificationEmail(email, function(err, userFound) {
+    if (err) {
+      // handle error...
+      console.log(err);
+    }
+      if (userFound){
+        // email has been sent
+        console.log('email was sent to' + email)
+      }
+      else{
+        // flash message of failure...
+        console.log('NO such user was found to resend the email verification to');
+      }
+  });
+})
+
+// End of email verification changes
+
 function sendEmail(mailOptions) {
   console.info('Send the email!');
   transporter.sendMail(mailOptions, (err, info) => {
@@ -86,15 +181,53 @@ router.post('/signup', (req, res) => {
         matchedBooks: [],
         univeristy: univeristy,
       });
-      newUser.save((errs, savedUser) => {
+      // More stuff for the email verification
+      nev.createTempUser(newUser, function(err, existingPersistentUser, newTempUser) {
+        // some sort of error
         if (err) {
-          return res.json(err);
-        } else {
-          sendEmail(signedUpEmail(savedUser.emailAddress, savedUser.firstName));
-          res.json("Sign up done!")
+          console.log(`User.js post error: ${err}`);
         }
-        return 0;
+        // user already exists in persistent collection...
+          if (existingPersistentUser){
+            res.json({
+              error: `Sorry, already a user with the username: ${emailAddress}`,
+            });
+          }
+
+          // a new user
+            if (newTempUser) {
+              var URL = newTempUser[nev.options.URLFieldName];
+              nev.sendVerificationEmail(emailAddress, URL, function(err, info) {
+                if (err) {
+                  return res.json(err);//email error
+                  // handle error...
+                }
+                console.log('email sent');
+                // flash message of success
+                  });
+              // user already exists in temporary collection...
+              console.log('user in temp collection');
+              res.json({
+                error: `Sorry, already a user with the username: ${emailAddress}`,
+              });
+
+            } else {
+              // flash message of failure...
+              console.log('failure; user.js');
+
+            }
       });
+      // End of new stuff for email verification
+      // Start of old stuff from creating the user,
+      // newUser.save((errs, savedUser) => {
+      //   if (err) {
+      //     return res.json(err);
+      //   } else {
+      //     sendEmail(signedUpEmail(savedUser.emailAddress, savedUser.firstName));
+      //     res.json("Sign up done!")
+      //   }
+      //   return 0;
+      // });
     }
   });
 });
