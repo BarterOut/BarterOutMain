dd266bcdc69e963bfd892706cee09f233bad3632
Duncan Grubbs <duncan.grubbs@gmail.com>
2018-09-30 11:43:50 -0400

    Fix.

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 const config = {
   mongoURL: process.env.MONGO_URL ||
-  'mongodb://BarterOut:LuisInnovation1@ds245150.mlab.com:45150/barterout-development',
+  'mongodb://BarterOutDev:LuisInnovation1@ds245150.mlab.com:45150/barterout-development',
   port: process.env.PORT || 8080,
 };
 
