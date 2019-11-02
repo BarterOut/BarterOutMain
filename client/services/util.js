dbd9ad3d2a1c9e62a67ae0240adfad3efca31ee7
Duncan Grubbs <duncan.grubbs@gmail.com>
2019-11-02 16:43:48 -0400

    Update util.js

--- client/services/util.js
+++ client/services/util.js
@@ -14,13 +14,13 @@ export default class Util {
   static ordinalSuffixOf(num) {
     const j = num % 10;
     const k = num % 100;
-    if (j == 1 && k != 11) {
+    if (j === 1 && k !== 11) {
       return `${num}st`;
     }
-    if (j == 2 && k != 12) {
+    if (j === 2 && k !== 12) {
       return `${num}nd`;
     }
-    if (j == 3 && k != 13) {
+    if (j === 3 && k !== 13) {
       return `${num}rd`;
     }
     return `${num}th`;
